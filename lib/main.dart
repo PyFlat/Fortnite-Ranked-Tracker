ba5ba56987989cd9026af3e2d6da6418a20519a3
Johannes Rößler <johannes.25406@gmail.com>
2024-07-29 01:20:05 +0200

    Only allow one instance running

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_single_instance/flutter_single_instance.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:tray_manager/tray_manager.dart';
 import 'package:window_manager/window_manager.dart';
@@ -10,7 +13,12 @@ import 'screens/auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Must add this line.
+  if (await FlutterSingleInstance.platform.isFirstInstance()) {
+    runApp(MyApp());
+  } else {
+    await setShowInstance(true);
+    exit(0);
+  }
   if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
     await windowManager.ensureInitialized();
     windowManager.setPreventClose(true);
@@ -32,6 +40,29 @@ void main() async {
   );
 }
 
+Future<String> getFilePath() async {
+  String directory = (await getTemporaryDirectory()).path;
+  return "$directory/fortnite-ranked-tracker-show.txt";
+}
+
+Future<void> setShowInstance(bool value) async {
+  final file = File(await getFilePath());
+  final content = value ? '1' : '0';
+  await file.writeAsString(content);
+}
+
+Future<bool> getShowInstance() async {
+  final file = File(await getFilePath());
+
+  if (await file.exists()) {
+    final content = await file.readAsString();
+    return content == '1';
+  } else {
+    await file.writeAsString('0');
+    return false;
+  }
+}
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -40,16 +71,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> with TrayListener, WindowListener {
+  late Timer timer;
+
   @override
   void initState() {
     trayManager.addListener(this);
     windowManager.addListener(this);
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+      final result = await getShowInstance();
+      if (result) {
+        windowManager.show();
+        await setShowInstance(false);
+      }
+    });
     _initSystemTray();
     super.initState();
   }
 
   @override
   void dispose() {
+    timer.cancel();
     trayManager.removeListener(this);
     windowManager.removeListener(this);
     super.dispose();
@@ -67,32 +108,30 @@ class _MyAppState extends State<MyApp> with TrayListener, WindowListener {
     Menu menu = Menu(
       items: [
         MenuItem(
-          key: 'show_window',
-          label: 'Show Window',
-        ),
-        MenuItem.separator(),
-        MenuItem(
           key: 'exit_app',
           label: 'Exit App',
         ),
       ],
     );
     if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
-      await trayManager.setContextMenu(menu);
       await trayManager.setIcon(iconPath);
+      await trayManager.setContextMenu(menu);
     }
   }
 
   @override
-  void onTrayIconMouseDown() {
+  void onTrayIconRightMouseDown() {
     trayManager.popUpContextMenu();
   }
 
   @override
+  void onTrayIconMouseDown() {
+    windowManager.show();
+  }
+
+  @override
   void onTrayMenuItemClick(MenuItem menuItem) {
-    if (menuItem.key == 'show_window') {
-      windowManager.show();
-    } else if (menuItem.key == 'exit_app') {
+    if (menuItem.key == 'exit_app') {
       windowManager.setPreventClose(false);
       windowManager.close();
     }
