68fe785daf33785ba07ebf05c794468f7e4c52f2
Johannes Rößler <johannes.25406@gmail.com>
2024-08-06 23:48:39 +0200

    added Talker Wrapper to show errors/exceptions in the app

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -106,130 +106,139 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      appBar: AppBar(
-        title: const Text('Fortnite Ranked Tracker'),
+    return TalkerWrapper(
+      talker: widget.talker,
+      options: const TalkerWrapperOptions(
+        enableErrorAlerts: true,
+        enableExceptionAlerts: true,
       ),
-      body: FutureBuilder<void>(
-        future: _initializationFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          } else {
-            return Center(child: _widgetOptions.elementAt(_selectedIndex));
-          }
-        },
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            FutureBuilder<void>(
-                future: _initializationFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          title: const Text('Fortnite Ranked Tracker'),
+        ),
+        body: FutureBuilder<void>(
+          future: _initializationFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            } else {
+              return Center(child: _widgetOptions.elementAt(_selectedIndex));
+            }
+          },
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              FutureBuilder<void>(
+                  future: _initializationFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return DrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple.shade400,
+                        ),
+                        child: const Row(children: [
+                          CircleAvatar(
+                              radius: 40, child: CircularProgressIndicator()),
+                        ]),
+                      );
+                    }
                     return DrawerHeader(
                       decoration: BoxDecoration(
                         color: Colors.deepPurple.shade400,
                       ),
-                      child: const Row(children: [
-                        CircleAvatar(
-                            radius: 40, child: CircularProgressIndicator()),
-                      ]),
-                    );
-                  }
-                  return DrawerHeader(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple.shade400,
-                    ),
-                    child: Row(
-                      children: [
-                        FutureBuilder<String>(
-                          future: RankService()
-                              .getAccountAvatar(), // Call the async function here
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              // Show a loading indicator while waiting
-                              return const CircleAvatar(
+                      child: Row(
+                        children: [
+                          FutureBuilder<String>(
+                            future: RankService()
+                                .getAccountAvatar(), // Call the async function here
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                  ConnectionState.waiting) {
+                                // Show a loading indicator while waiting
+                                return const CircleAvatar(
+                                    radius: 40,
+                                    child: CircularProgressIndicator());
+                              } else if (snapshot.hasError) {
+                                // Handle errors
+                                return const Icon(Icons.error,
+                                    color: Colors.red);
+                              } else if (snapshot.hasData) {
+                                // Display the image once the URL is fetched
+                                return CircleAvatar(
                                   radius: 40,
-                                  child: CircularProgressIndicator());
-                            } else if (snapshot.hasError) {
-                              // Handle errors
-                              return const Icon(Icons.error, color: Colors.red);
-                            } else if (snapshot.hasData) {
-                              // Display the image once the URL is fetched
-                              return CircleAvatar(
-                                radius: 40,
-                                backgroundImage: NetworkImage(snapshot.data!),
-                              );
-                            } else {
-                              // Handle the case where there's no data
-                              return const Icon(Icons.image,
-                                  color: Colors.grey);
-                            }
-                          },
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Text(
-                            widget.authProvider.displayName,
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
+                                  backgroundImage: NetworkImage(snapshot.data!),
+                                );
+                              } else {
+                                // Handle the case where there's no data
+                                return const Icon(Icons.image,
+                                    color: Colors.grey);
+                              }
+                            },
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Text(
+                              widget.authProvider.displayName,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    );
+                  }),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: <Widget>[
+                    ListTile(
+                      leading:
+                          const Icon(Icons.dashboard, color: Colors.blueGrey),
+                      title: const Text('Dashboard',
+                          style: TextStyle(fontSize: 16)),
+                      onTap: () => _onItemTapped(0),
                     ),
-                  );
-                }),
-            Expanded(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  ListTile(
-                    leading:
-                        const Icon(Icons.dashboard, color: Colors.blueGrey),
-                    title:
-                        const Text('Dashboard', style: TextStyle(fontSize: 16)),
-                    onTap: () => _onItemTapped(0),
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.trending_up_rounded,
-                        color: Colors.blueGrey),
-                    title: const Text('Graph', style: TextStyle(fontSize: 16)),
-                    onTap: () => _onItemTapped(1),
-                  ),
-                  _buildDatabaseListTile()
-                ],
+                    ListTile(
+                      leading: const Icon(Icons.trending_up_rounded,
+                          color: Colors.blueGrey),
+                      title:
+                          const Text('Graph', style: TextStyle(fontSize: 16)),
+                      onTap: () => _onItemTapped(1),
+                    ),
+                    _buildDatabaseListTile()
+                  ],
+                ),
               ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.my_library_books_rounded,
-                  color: Colors.blueGrey),
-              title: const Text('Logs', style: TextStyle(fontSize: 16)),
-              onTap: () => {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            TalkerScreen(talker: widget.talker)))
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings, color: Colors.blueGrey),
-              title: const Text('Settings', style: TextStyle(fontSize: 16)),
-              onTap: () => {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SettingsScreen()))
-              },
-            ),
-          ],
+              ListTile(
+                leading: const Icon(Icons.my_library_books_rounded,
+                    color: Colors.blueGrey),
+                title: const Text('Logs', style: TextStyle(fontSize: 16)),
+                onTap: () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              TalkerScreen(talker: widget.talker)))
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings, color: Colors.blueGrey),
+                title: const Text('Settings', style: TextStyle(fontSize: 16)),
+                onTap: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SettingsScreen()))
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
