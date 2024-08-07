24c0f91a448be1f90b37c7fd614b75083e62dd75
Johannes Rößler <johannes.25406@gmail.com>
2024-08-07 23:32:06 +0200

    Added Settings Ui Package

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_ui/flutter_settings_ui.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('Settings'),
-    ));
+      appBar: AppBar(
+        title: Text('Test'),
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(title: Text("Test"), tiles: [
+            SettingsTile(
+              leading: Icon(Icons.emoji_people),
+              title: Text("Test"),
+            )
+          ])
+        ],
+      ),
+    );
   }
 }
